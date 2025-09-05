ed1c5d1635353df32baf160a40e3e9e45ae73623
Dante <dantezulli2004@gmail.com>
2025-09-05 20:19:27 -0300

    Add login activity layout and implementation

---
+++ app/src/main/java/com/example/tp_kotlin_grupo_v/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tp_kotlin_grupo_v
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
